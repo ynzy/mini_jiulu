89c4efc4ab968791fbd17aed3f006d838fe55520
zhangyong <zhangyong@163.com>
2019-09-25 14:39:08 +0800

    feat(module:all): 小程序之间跳转

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,53 +1,20 @@
-// pages/my/my.js
+// pages/about/about.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo: null,
-    authorized: false,
-    myBooksCount: 0
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.userAuthorized()
 
   },
-  // 用户认证
-  userAuthorized() {
-    wx.getSetting({
-      success: (data) => {
-        if (data.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            lang: 'zh_CN',
-            success: (data) => {
-              // console.log(data);
-              this.setData({authorized: true, userInfo:data.userInfo})
-            },
-          });
-        } else { 
-          
-        }
-      },
-    });
-  },
-  // 获取用户信息
-  onGetUserInfo(event) {
-    const {userInfo} = event.detail
-    // console.log(userInfo);
-    if(userInfo) {
-      this.setData({userInfo,authorized: true})
-    }
-  },
-  onJumpToAbout(event) {
-    wx.navigateTo({
-      url: '/pages/about/about',
-    });
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
